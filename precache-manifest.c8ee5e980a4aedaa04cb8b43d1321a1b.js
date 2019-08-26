4165e6186f807b9f726b2d1c599cbfd1c268d298
Ignacio Petrone <ipetrone@Santiagos-MacBook-Pro.local>
2019-08-26 23:30:25 +0200

    Updates

---
+++ precache-manifest.c8ee5e980a4aedaa04cb8b43d1321a1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4a8bb395eb017f2d3a827146cdf790",
+    "url": "/swarovski/index.html"
+  },
+  {
+    "revision": "2b0d7c6600b4f3618759",
+    "url": "/swarovski/static/css/main.d1aa59cb.chunk.css"
+  },
+  {
+    "revision": "a4855e45b06481297cbf",
+    "url": "/swarovski/static/js/2.cb2fa9f2.chunk.js"
+  },
+  {
+    "revision": "2b0d7c6600b4f3618759",
+    "url": "/swarovski/static/js/main.6bcf2cfa.chunk.js"
+  },
+  {
+    "revision": "302eabe26fa1c36f47e1",
+    "url": "/swarovski/static/js/runtime~main.c7b59218.js"
+  }
+]);
